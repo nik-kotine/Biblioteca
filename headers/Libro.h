862ce677b8ea06828109ba59ba040e37e14d804b
nik-kotine <mswv280407@gmail.com>
2025-03-28 18:38:53 -0500

    ...

--- headers/Libro.h
+++ headers/Libro.h
@@ -1,1 +1,8 @@
-
+class Libro {
+	std::string titulo;
+	std::string autor;
+	int anyo;
+public:
+	Libro(std::string _titulo, std::string _autor, int _anyo);
+	void mostrarInformacion();
+}
--- src/Libro.cpp
+++ src/Libro.cpp
@@ -1,10 +1,5 @@
-class Libro {
-	std::string titulo;
-	std::string autor;
-	int anyo;
-public:
-	Libro(std::string _titulo, std::string _autor, int _anyo) {
-		titulo = _titulo; autor = _autor; anyo = _anyo;
-	}
-	mostrarLibro() { std::cout << titulo << "(" << anyo << ") by: " << autor << "\n";  }
+#include "Libro.h"
+
+Libro::Libro(std::string _titulo, std::string _autor, int _anyo) { 
+	titulo = _titulo; autor = _autor; anyo = _anyo;
 }
